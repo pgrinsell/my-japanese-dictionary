d72bba125c6c6f6d088412b3635ff092a763da12
Patrick Grinsell <pgrinsell@outlook.com>
2023-11-14 14:55:15 +0000

    Adds search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
 import '@fontsource/inter';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import Typography from '@mui/joy/Typography';
+import Stack from '@mui/joy/Stack';
+import Input from '@mui/joy/Input';
+import { useState, useEffect, useRef } from 'react';
+import { toKana, isKana } from 'wanakana';
+// import Search from '@mui/icons-material/Search';
+
+function SearchBar({ onSearch }) {
+  const [value, setValue] = useState('');
+  const [mode, setMode] = useState('en');
+  const ref = useRef();
+
+  function handleSetValue(newValue) {
+    console.log(`isKana: ${isKana(newValue)}`);
+
+    if (mode === 'jp') {
+      const valueAsKana = toKana(newValue, { IMEMode: true });
+      setValue(valueAsKana);
+    } else {
+      setValue(newValue);
+    }
+  }
+  
+  function handleSearch(event) {
+    event.preventDefault();
+    onSearch(value);
+    setValue('');
+  }
+
+  function handleChangeMode() {
+    setMode(currentMode => currentMode === 'en' ? 'jp' : 'en');
+    setValue('');
+    ref.current.focus();
+  }
+
+  return (
+    <form onSubmit={handleSearch}>
+      <Input
+        slotProps={{ input: { ref } }}
+        size="lg"
+        variant="outlined"
+        placeholder="Search for a word or phrase..."
+        autoFocus
+        value={value}
+        onChange={e => handleSetValue(e.target.value)}
+        startDecorator={(
+          <Button
+            variant="soft"
+            // color="neutral"
+            onClick={e => handleChangeMode(e)}
+          >
+            {mode === 'en' ? 'EN -> JP' : 'JP -> EN'}
+          </Button>
+        )}
+        endDecorator={(
+          <Button variant="soft" color="plain" type="submit">
+            Search
+          </Button>
+        )}
+      />
+    </form>
+  );
+}
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (Boolean(search)) {
+      setResults([search, ...results]);
+    }
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CssVarsProvider>
+      <CssBaseline />
+      <Stack spacing={2} sx={{ m: 2 }}>
+        <SearchBar onSearch={value => setSearch(value)} />
+        {results.length > 0 ? results.map((result, i) => (
+          <Card key={i}>
+            <Typography>{result}</Typography>
+          </Card>
+        )) : (
+          <>
+            <Typography>
+              Search for a world or phrase using the input above. You can switch mode between English to Japanese and Japanese to English. Japanese to English mode supports Kana input.
+            </Typography>
+            <Typography>
+              Your search results will appear in this area.
+            </Typography>
+          </>
+        )}
+      </Stack>
+    </CssVarsProvider>
   );
 }
 
