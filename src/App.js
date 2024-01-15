cdbe06f399b2adcc2eea902e5b0ee3ac86ddb5ed
Patrick Grinsell <pgrinsell@outlook.com>
2024-01-15 15:25:36 +0000

    Replaces local file with JMdict

--- src/App.js
+++ src/App.js
@@ -6,38 +6,77 @@ import Card from '@mui/joy/Card';
 import Typography from '@mui/joy/Typography';
 import Stack from '@mui/joy/Stack';
 import { useState, useEffect } from 'react';
-import data from './data.json';
-import parse from 'html-react-parser';
-import Grid from '@mui/joy/Grid';
+// import data from './JMdict_e.json';
+// import parse from 'html-react-parser';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
 import SearchBar from './SearchBar';
+import data from './jmdict-eng-common-3.5.0.json';
+
+const { words } = data;
+
+// const entries = data.JMdict.entry;
 
 // function handleAddFuriGana(text) {
 //   // todo replace the whole block between perenthesis to avoid orphaned tags
 //   return parse(`<ruby>${text.replaceAll('(', '<rt>').replaceAll(')', '</rt>')}</ruby>`);
 // }
 
-function ResultCard({ en, kanji, kana }) {
+function Reading({ kanji, kana }) {
+  if (kanji) {
+    return (
+      <ruby>
+        {kanji}
+        <rt>{kana}</rt>
+      </ruby>
+    );
+  }
+
+  return kana;
+};
+
+function ResultCard({ kanji, kana, definitions }) {
   return (
     <Card>
-      <Grid container spacing={4}>
-        <Grid sx={{ width: 200 }}>
-          <Typography level="h2" sx={{ fontWeight: 'normal' }}>
-            {/* {handleAddFuriGana(result.jp)} */}
-            <ruby>
-              {kanji}
-              <rt>{kana}</rt>
-            </ruby>
-          </Typography>
-        </Grid>
-        <Grid>
-          <Typography>
-            {en}
-          </Typography>
-        </Grid>
-      </Grid>
+      <Typography level="h2" sx={{ fontWeight: 'normal' }}>
+        <Reading kanji={kanji} kana={kana} />
+      </Typography>
+      <List component="ol" marker="decimal">
+        {definitions.map(d => (
+          <ListItem>{d}</ListItem>
+        ))}
+      </List>
     </Card>
   );
 }
+
+function handleSearch(mode, value) {
+  if (!Boolean(value)) {
+    return [];
+  }
+
+  if (mode === 'en') {
+    const lcValue = value.toLocaleLowerCase();
+
+    return words.filter(word => {
+      try {
+        const { sense } = word;
+        return sense.some(({ gloss }) => gloss.some(({ text }) => text.includes(lcValue)));
+      } catch (ex) {
+        console.error('error searching entry', ex.message, word);
+        return false;
+      }
+    });
+
+    // todo implement more complex search
+  } else if (mode === 'jp') {
+    return words.filter(({ kanji, kana }) => kanji.includes(value) || kana.includes(value));
+  }
+};
+
+
+
+
 
 function App() {
   const [search, setSearch] = useState({ value: '', mode: '' });
@@ -45,16 +84,23 @@ function App() {
 
   useEffect(() => {
     const { value, mode } = search;
+    const results = handleSearch(mode, value);
+    console.log(results);
 
-    if (Boolean(value)) {
-      if (mode === 'en') {
-        // todo implement more complex search
-        setResults(data.filter(({ en }) => en.toLocaleLowerCase().includes(value.toLocaleLowerCase())));
-      } else if (mode === 'jp') {
-        setResults(data.filter(({ kanji, kana }) => kanji.includes(value) || kana.includes(value)));
-      }
-    }
+    // todo ignoring other kanji/kana forms for now
+
+    const newResults = results.map(({ kanji, kana, sense }) => ({
+      kanji: kanji.map(({ text }) => text)[0],
+      kana: kana.map(({ text }) => text)[0],
+      definitions: sense.map(({ gloss }) => gloss.map(({ text }) => text).join(', '))
+    }));
+
+    setResults(newResults);
   }, [search]);
+
+  useEffect(() => {
+    console.log(results);
+  }, [results]);
 
   return (
     <CssVarsProvider>
