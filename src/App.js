8fa0f68680c382a1e84c68844d55a7b1e7095e60
Patrick Grinsell <pgrinsell@outlook.com>
2024-01-16 07:39:12 +0000

    Fixes kana search and tidies up code

--- src/App.js
+++ src/App.js
@@ -15,24 +15,19 @@ import data from './jmdict-eng-common-3.5.0.json';
 
 const { words } = data;
 
-// const entries = data.JMdict.entry;
-
-// function handleAddFuriGana(text) {
-//   // todo replace the whole block between perenthesis to avoid orphaned tags
-//   return parse(`<ruby>${text.replaceAll('(', '<rt>').replaceAll(')', '</rt>')}</ruby>`);
-// }
-
 function Reading({ kanji, kana }) {
-  if (kanji) {
+  if (kanji.length > 0) {
+    // todo ignoring other kanji/kana forms for now
+
     return (
       <ruby>
-        {kanji}
-        <rt>{kana}</rt>
+        {kanji[0]}
+        <rt>{kana[0]}</rt>
       </ruby>
     );
   }
 
-  return kana;
+  return kana[0];
 };
 
 function ResultCard({ kanji, kana, definitions }) {
@@ -42,8 +37,8 @@ function ResultCard({ kanji, kana, definitions }) {
         <Reading kanji={kanji} kana={kana} />
       </Typography>
       <List component="ol" marker="decimal">
-        {definitions.map(d => (
-          <ListItem>{d}</ListItem>
+        {definitions.map((d, i) => (
+          <ListItem key={i}>{d}</ListItem>
         ))}
       </List>
     </Card>
@@ -51,32 +46,32 @@ function ResultCard({ kanji, kana, definitions }) {
 }
 
 function handleSearch(mode, value) {
-  if (!Boolean(value)) {
-    return [];
+  let results = [];
+
+  if (Boolean(value)) {
+
+    // todo implement more inteligent search, it currently has several problems:
+    // 1. if search has many results it will lag returning a large data set (could be fixed with lazy loading/pagination)
+    // 2. basic include will mean the term "hello" returns words relating to "othello" for example
+    //    this could ignore partial matches on the data side so "hello" must be at the beginning or preceeded by a space
+    // 3. the order does not relate to how exact/good/common the match is, for example the first result for "hello" is "どうも" instead of "こんにちは"
+    //    this could take into account the definition number, i.e. highest priority for definition 1
+
+    if (mode === 'en') {
+      const lcValue = value.toLocaleLowerCase();
+      results = words.filter(word => word.sense.some(({ gloss }) => gloss.some(({ text }) => text.includes(lcValue))));
+    } else if (mode === 'jp') {
+      results = words.filter(({ kanji, kana }) => kanji.some(({ text }) => text.includes(value)) || kana.some(({ text }) => text.includes(value)));
+    }
   }
 
-  if (mode === 'en') {
-    const lcValue = value.toLocaleLowerCase();
-
-    return words.filter(word => {
-      try {
-        const { sense } = word;
-        return sense.some(({ gloss }) => gloss.some(({ text }) => text.includes(lcValue)));
-      } catch (ex) {
-        console.error('error searching entry', ex.message, word);
-        return false;
-      }
-    });
-
-    // todo implement more complex search
-  } else if (mode === 'jp') {
-    return words.filter(({ kanji, kana }) => kanji.includes(value) || kana.includes(value));
-  }
+  return results
+    .map(({ kanji, kana, sense }) => ({
+      kanji: kanji.map(({ text }) => text),
+      kana: kana.map(({ text }) => text),
+      definitions: sense.map(({ gloss }) => gloss.map(({ text }) => text).join(', '))
+    }));
 };
-
-
-
-
 
 function App() {
   const [search, setSearch] = useState({ value: '', mode: '' });
@@ -85,17 +80,7 @@ function App() {
   useEffect(() => {
     const { value, mode } = search;
     const results = handleSearch(mode, value);
-    console.log(results);
-
-    // todo ignoring other kanji/kana forms for now
-
-    const newResults = results.map(({ kanji, kana, sense }) => ({
-      kanji: kanji.map(({ text }) => text)[0],
-      kana: kana.map(({ text }) => text)[0],
-      definitions: sense.map(({ gloss }) => gloss.map(({ text }) => text).join(', '))
-    }));
-
-    setResults(newResults);
+    setResults(results);
   }, [search]);
 
   useEffect(() => {
